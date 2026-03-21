c70ed5d1bbefe24a444b096ba3581dcf5bcfa912
Boccoli Filippo <st10784@ITIS.JESI>
2026-03-21 10:00:32 +0100

    Codice bottoni

--- app/src/main/java/com/example/calcolatrice_boccoli/MainActivity.java
+++ app/src/main/java/com/example/calcolatrice_boccoli/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.calcolatrice_boccoli;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+
+    Button btMoltiplicazione, btDivisione, btAddizione, btSottrazione;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        button0 = findViewById(R.id.btn0);
+        button1 = findViewById(R.id.btn1);
+        button2 = findViewById(R.id.btn2);
+        button3 = findViewById(R.id.btn3);
+        button4 = findViewById(R.id.btn4);
+        button5 = findViewById(R.id.btn5);
+        button6 = findViewById(R.id.btn6);
+        button7 = findViewById(R.id.btn7);
+        button8 = findViewById(R.id.btn8);
+        button9 = findViewById(R.id.btn9);
+
+        btAddizione = findViewById(R.id.btnPlus);
+        btSottrazione = findViewById(R.id.btnMinus);
+        btMoltiplicazione = findViewById(R.id.btnMultiply);
+        btDivisione = findViewById(R.id.btnDivide);
+
+
+    }
+
+    
+
+    @Override
+    public <T extends View> T findViewById(int id) {
+        return super.findViewById(id);
     }
 }
